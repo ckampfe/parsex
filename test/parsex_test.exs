d2b1f3d14e2f4c57befae95a0cf1bb3990b83fc6
Clark Kampfe <clark@bellycard.com>
2015-08-30 10:38:15 -0500

    rename to top level module

--- test/parsex_test.exs
+++ test/parsex_test.exs
@@ -1,3 +1,77 @@
 defmodule ParsexTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  import Parsex
+
+  test "string literal parsing" do
+    assert({:ok, ""} = lit("foo").("foo"))
+    assert({:error, _} = lit("foo").("bar"))
+    assert({:ok, " again"} = lit("we shall meet").("we shall meet again"))
+  end
+
+  test "regex parsing" do
+    assert({:ok, ""} = pregex(~r/^\w{3} as easy as \d{3}/).("abc as easy as 123"))
+  end
+
+  test "or parsing" do
+    assert(
+      {:ok, ""} = por(
+        [
+          lit("foo"),
+          lit("bar"),
+          lit("baz")
+        ]
+      ).("baz")
+    )
+
+    assert(
+      {:error, _} = por(
+        [
+          lit("foo"),
+          lit("bar"),
+          lit("baz")
+        ]
+      ).("quux")
+    )
+  end
+
+  test "and parsing" do
+    assert(
+      {:ok, ""} = pand(
+        [
+          lit("foo"),
+          lit("bar")
+        ]
+      ).("foo bar")
+    )
+
+    # completely parses given input
+    assert(
+      {:ok, ""} = pand(
+        [
+          lit("foo"),
+          pand([lit("bar"), lit("baz"), lit("quux")])
+        ]
+      ).("foo bar baz quux")
+    )
+
+    # successfully parses, leaving some string leftover
+    assert(
+      {:ok, " quux"} = pand(
+        [
+          lit("foo"),
+          pand([lit("bar"), lit("baz")])
+        ]
+      ).("foo bar baz quux")
+    )
+
+    # doesn't have a third word
+    assert(
+      {:error, _} = pand(
+        [
+          lit("foo"),
+          pand([lit("bar"), pregex(~r/\w+/)])
+        ]
+      ).("foo bar")
+    )
+  end
 end
